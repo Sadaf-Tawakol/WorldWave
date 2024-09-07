afb3cf9c0b9613051485b7201d1c461579ddfc05
KarimaHabibi <162191258+KarimaHabibi@users.noreply.github.com>
2024-09-07 19:37:33 +0430

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+document
+  .getElementById("profile-input")
+  .addEventListener("change", function (event) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        document.getElementById("profile-image").src = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  });
+
+document.querySelector(".cancel-btn").addEventListener("click", function () {
+  alert("Cancelled");
+});
+
+document
+  .querySelector(".profile-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Profile Saved!");
+  });
 
